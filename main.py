d92c1971bfd2b4c0b24fae03de1da2cc9357c04f
alevere <alevere@email.unc.edu>
2021-06-22 15:06:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,25 +7,19 @@ app = flask.Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def server_error():
-   """ Return a friendly HTTP error. """
-   print('Status: 500 Internal Server Error')
-   print('Content-Type: application/openc2-rsp+json;version=1.0')
-   print('')
-   print(json.dumps({'status': (500), 'status_text': 'Server error'}, sort_keys=True, indent=4))
-   print('')
-   return 0
+   return '500'
 
 @app.route('/openc2', methods=['GET','POST'])
 def index():
-    @after_this_request
-    def add_header(response):
-        response.headers['X-Foo'] = 'Parachute'
-        return response
-    return 'Hello World!'
+   @after_this_request
+   def add_header(response):
+      response.headers['X-Foo'] = 'Parachute'
+      return response
+   return 'Hello World!'
 
 
 if __name__ == '__main__':
-    # Used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    app.run(host="localhost", port=8080, debug=True)
+   # Used when running locally only. When deploying to Google App
+   # Engine, a webserver process such as Gunicorn will serve the app. This
+   # can be configured by adding an `entrypoint` to app.yaml.
+   app.run(host="localhost", port=8080, debug=True)
