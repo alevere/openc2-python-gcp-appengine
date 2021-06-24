0326bce89d04bc940ef87aa67d42c0411e2b30c2
alevere <alevere@email.unc.edu>
2021-06-24 09:10:52 -0400

    Adding content from slpf implementation

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import flask
 import urllib.request
 import json
+import subprocess
+import os
+import sys
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
 app = flask.Flask(__name__)
@@ -11,10 +14,21 @@ def server_error():
 
 @app.route('/openc2', methods=['GET','POST'])
 def index():
-    data = json.dumps({'status': (200), 'status_text': 'success'}, sort_keys=True, indent=4)
+    consumer_response=server_200('complete',json.loads('{"status":200}'))
+    data = json.dumps(consumer_response, sort_keys=True, indent=4)
     response = app.response_class(response=data,status=200,mimetype='application/json')
+    response.headers['Access-Control-Allow-Origin'] = '*'
     return response
-
+   
+# tell producer things were ok
+def server_200(complete,resultant):
+   if complete=="complete":
+      return json.dumps(resultant, sort_keys=True, indent=4)
+   elif complete=="none":
+      return
+   else:
+      return json.loads('{"status":200}')
+   
 if __name__ == '__main__':
    # Used when running locally only. When deploying to Google App
    # Engine, a webserver process such as Gunicorn will serve the app. This
